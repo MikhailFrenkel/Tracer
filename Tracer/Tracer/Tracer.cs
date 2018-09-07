9bcbc00154ea8e6dda3efe535bfbbaaa8e823117
MikhailFrenkel <frenkel.misha@mail.ru>
2018-09-07 23:14:01 +0300

    Implemented a class tracer

---
+++ Tracer/Tracer/Tracer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+
+namespace Tracer
+{
+    public struct TraceResult
+    {
+        public string MethodName;
+        public string ClassName;
+        public long TimeExecution;
+    }
+
+    public class CustomTracer : ITracer
+    {
+        private TraceResult _traceResult;
+        private Stopwatch _stopwatch;
+
+        public void StartTrace()
+        {
+            StackFrame frame = new StackFrame(1);
+            MethodBase method = frame.GetMethod();
+            _traceResult.MethodName = method.Name;
+            _traceResult.ClassName = method.DeclaringType?.Name;
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
+        public void StopTrace()
+        {
+            _stopwatch.Stop();
+            _traceResult.TimeExecution = _stopwatch.ElapsedMilliseconds;
+        }
+
+        public TraceResult GetTraceResult()
+        {
+            return _traceResult;
+        }
+    }
+}
--- Tracer/UsingTracerConsoleApp/Program.cs
+++ Tracer/UsingTracerConsoleApp/Program.cs
@@ -2,15 +2,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Tracer;
 
 namespace UsingTracerConsoleApp
 {
     class Program
     {
+        private static ITracer _tracer;
+
         static void Main(string[] args)
         {
-            
+            _tracer = new CustomTracer();
+            SomeMethod();
+            TraceResult traceResult = _tracer.GetTraceResult();
+            Console.WriteLine($"Class: {traceResult.ClassName}\n" +
+                              $"Method: {traceResult.MethodName}\n" +
+                              $"Time: {traceResult.TimeExecution}ms");
+            Console.ReadLine();
+        }
+
+        private static void SomeMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(1000);
+            _tracer.StopTrace();
         }
     }
 }
