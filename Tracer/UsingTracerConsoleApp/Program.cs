e0cc8e7a83fe296a4ee75d1e19e67c543eebfb83
MikhailFrenkel <frenkel.misha@mail.ru>
2018-09-10 00:37:23 +0300

    added threads list

--- Tracer/UsingTracerConsoleApp/Program.cs
+++ Tracer/UsingTracerConsoleApp/Program.cs
@@ -10,23 +10,48 @@ namespace UsingTracerConsoleApp
 {
     class Program
     {
-        private static ITracer _tracer;
+        private static CustomTracer _tracer;
+        private static List<Thread> _threads;
 
         static void Main(string[] args)
         {
+            _threads = new List<Thread>();
             _tracer = new CustomTracer();
-            SomeMethod();
-            TraceResult traceResult = _tracer.GetTraceResult();
-            Console.WriteLine($"Class: {traceResult.ClassName}\n" +
-                              $"Method: {traceResult.MethodName}\n" +
-                              $"Time: {traceResult.TimeExecution}ms");
+            MultipleThreadMethod();
+            //TraceResult traceResult = _tracer.GetTraceResult();
+            //Console.WriteLine($"Class: {traceResult.ClassName}\n" +
+            //                  $"Method: {traceResult.MethodName}\n" +
+            //                  $"Time: {traceResult.TimeExecution}ms");
             Console.ReadLine();
+        }
+
+        private static void MultipleThreadMethod()
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                Thread thread = new Thread(SomeMethod) {IsBackground = true};
+                _threads.Add(thread);
+                thread.Start();
+            }
+
+            foreach (Thread t in _threads)
+            {
+                t.Join();
+            }
         }
 
         private static void SomeMethod()
         {
             _tracer.StartTrace();
             Thread.Sleep(1000);
+            Calculation();
+            _tracer.StopTrace();
+        }
+
+        private static void Calculation()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(500);
             _tracer.StopTrace();
         }
     }
