3ad085891338d042abdce60d8f4ff5336d7249f7
MikhailFrenkel <frenkel.misha@mail.ru>
2018-09-09 23:51:33 +0300

    deleted StopTracing event

--- Tracer/Tracer/Tracer.cs
+++ Tracer/Tracer/Tracer.cs
@@ -3,39 +3,37 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 
 namespace Tracer
 {
-    public delegate void TraceDelegate(object sender, TracerEventArgs e);
-
-    public struct TraceResult
-    {
-        public string MethodName;
-        public string ClassName;
-        public long TimeExecution;
-    }
-
     public class CustomTracer : ITracer
     {
-        public event TraceDelegate OnStopTracing; 
         private TraceResult _traceResult;
-        private Stopwatch _stopwatch;
+
+        public CustomTracer()
+        {
+            _traceResult = new TraceResult();
+        }
 
         public void StartTrace()
         {
             StackFrame frame = new StackFrame(1);
             MethodBase method = frame.GetMethod();
-            _traceResult.MethodName = method.Name;
-            _traceResult.ClassName = method.DeclaringType?.Name;
-            _stopwatch = new Stopwatch();
-            _stopwatch.Start();
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            
+
+            
+            //_traceResult.MethodName = method.Name;
+            //_traceResult.ClassName = method.DeclaringType?.Name;
+            //_stopwatch = new Stopwatch();
+            //_stopwatch.Start();
         }
 
         public void StopTrace()
         {
-            _stopwatch.Stop();
-            _traceResult.TimeExecution = _stopwatch.ElapsedMilliseconds;
-            OnStopTracing?.Invoke(this, new TracerEventArgs(_traceResult));
+            //_stopwatch.Stop();
+            //_traceResult.TimeExecution = _stopwatch.ElapsedMilliseconds;
         }
 
         public TraceResult GetTraceResult()
