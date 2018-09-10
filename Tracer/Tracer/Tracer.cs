6a74be60d80f515fa0694111602994d6ba412b50
MikhailFrenkel <frenkel.misha@mail.ru>
2018-09-10 22:30:48 +0300

    changed property name

--- Tracer/Tracer/Tracer.cs
+++ Tracer/Tracer/Tracer.cs
@@ -1,33 +1,27 @@
 ﻿using System.Diagnostics;
 using System.Reflection;
-using Newtonsoft.Json;
 
 namespace Tracer
 {
     public class CustomTracer : ITracer
     {
-        public TraceResult _traceResult { get; }
+        public TraceResult TraceResult { get; }
 
         public CustomTracer()
         {
-            _traceResult = new TraceResult();
+            TraceResult = new TraceResult();
         }
 
         public void StartTrace()
         {
             StackFrame frame = new StackFrame(1);
             MethodBase method = frame.GetMethod();
-            _traceResult.StartMethodTrace(method);
+            TraceResult.StartMethodTrace(method);
         }
 
         public void StopTrace()
         {
-            _traceResult.StopMethodTrace();
-        }
-
-        public TraceResult GetTraceResult()
-        {
-            return _traceResult;
+            TraceResult.StopMethodTrace();
         }
     }
 }
